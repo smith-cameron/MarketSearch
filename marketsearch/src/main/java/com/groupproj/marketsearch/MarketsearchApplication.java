7aca8181d1d249e1eeb7d70f7208fecf06623941
Cameron Smith <theauspiciouscammy@gmail.com>
2021-03-25 09:18:46 -0700

    empty SpringBoot project

---
+++ marketsearch/src/main/java/com/groupproj/marketsearch/MarketsearchApplication.java
@@ -0,0 +1,13 @@
+package com.groupproj.marketsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketsearchApplication.class, args);
+	}
+
+}
