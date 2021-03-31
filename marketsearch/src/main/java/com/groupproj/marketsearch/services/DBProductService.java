356b12a0b0253f11976681656af925c0955a6eba
Cameron Smith <theauspiciouscammy@gmail.com>
2021-03-31 08:56:38 -0700

    DBProduct Services

--- marketsearch/src/main/java/com/groupproj/marketsearch/services/DBProductService.java
+++ marketsearch/src/main/java/com/groupproj/marketsearch/services/DBProductService.java
@@ -12,21 +12,33 @@ public class DBProductService {
 	@Autowired
 	private DBProductRepo dbpRepo;
 	
+	public DBProduct createEntry(DBProduct input) {
+		return this.dbpRepo.save(input);
+	}
+	public List<DBProduct> getAll(){
+		return this.dbpRepo.findAll();
+	}
+	public DBProduct getById(Long id) {
+		return this.dbpRepo.findById(id).orElse(null);
+	}
+	public void deleteById(Long id) {
+		this.dbpRepo.deleteById(id);
+	}
 	//Create Product object to save to wishlist
-		public DBProduct createEntry(String title, Double basePrice, Double bestPrice, String barcode) {
-			DBProduct newEntry = new DBProduct(title, basePrice, bestPrice, barcode);
-			return this.dbpRepo.save(newEntry);
-		}
-		//Add to wishlist
-		public void wish(DBProduct product, User user) {
-			List<User> prodDesired = product.getUsersWhoWant();
-			prodDesired.add(user);
-			this.dbpRepo.save(product);
-		}
-		//Remove from wishlist
-		public void unWish(DBProduct product, User user) {
-			List<User> prodDesired = product.getUsersWhoWant();
-			prodDesired.remove(user);
-			this.dbpRepo.save(product);
-		}
+	public DBProduct createEntry(String title, Double basePrice, Double bestPrice, String barcode) {
+		DBProduct newEntry = new DBProduct(title, basePrice, bestPrice, barcode);
+		return this.dbpRepo.save(newEntry);
+	}
+	//Add to wishlist
+	public void wish(DBProduct product, User user) {
+		List<User> prodDesired = product.getUsersWhoWant();
+		prodDesired.add(user);
+		this.dbpRepo.save(product);
+	}
+	//Remove from wishlist
+	public void unWish(DBProduct product, User user) {
+		List<User> prodDesired = product.getUsersWhoWant();
+		prodDesired.remove(user);
+		this.dbpRepo.save(product);
+	}
 }
